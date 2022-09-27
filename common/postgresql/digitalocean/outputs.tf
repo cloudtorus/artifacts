2395c98bd1a1a15019353cf3400ba12169bbab7e
Shukant Pal <sukantk2002@outlook.com>
2022-09-26 21:43:21 -0400

    sensitive output digitalocean

--- common/postgresql/digitalocean/outputs.tf
+++ common/postgresql/digitalocean/outputs.tf
@@ -4,4 +4,5 @@ output "pgsql_db_cluster" {
 
 output "pgsql_db_uri" {
   value = "postgresql://${digitalocean_database_user.app.name}:${digitalocean_database_user.app.password}@${digitalocean_database_connection_pool.main.private_host}/${digitalocean_database_db.main.name}"
+  sensitive = true
 }
