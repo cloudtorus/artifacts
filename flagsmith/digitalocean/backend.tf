e748cf7fc183c028e1a05203d0d46255fc802862
Shukant Pal <sukantk2002@outlook.com>
2022-10-09 14:48:15 -0400

    Finish k8s

--- flagsmith/digitalocean/backend.tf
+++ flagsmith/digitalocean/backend.tf
@@ -3,7 +3,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check = true
     endpoint = "https://nyc3.digitaloceanspaces.com"
-    region = "us-east-1"
     key = "flagsmith.terraform.tfstate"
   }
 }
