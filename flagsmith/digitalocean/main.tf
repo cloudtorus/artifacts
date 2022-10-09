21bb72bb7046baaf268201a12a5e9942dab889ef
Shukant Pal <sukantk2002@outlook.com>
2022-10-08 20:20:27 -0400

    Pause work

--- flagsmith/digitalocean/main.tf
+++ flagsmith/digitalocean/main.tf
@@ -1,31 +1,30 @@
 data "terraform_remote_state" "pgsql_db" {
   backend = "s3"
-  config {
+  config = {
     bucket = var.backend_bucket
-    prefix = "state/pgsql"
+    prefix = "postgresql.terraform.tfstate"
   }
 }
 
 data "terraform_remote_state" "k8s" {
   backend = "s3"
-  config {
+  config = {
     bucket = var.backend_bucket
-    prefix = "state/k8s"
+    key = "k8s.terraform.tfstate"
   }
 }
 
 data "digitalocean_kubernetes_cluster" "main" {
-  name = data.terraform_remote_state.k8s.k8s_cluster_name
-  location = data.terraform_remote_state.k8s.k8s_cluster_region
+  name = data.terraform_remote_state.k8s.outputs.k8s_cluster_name
 }
 
 provider "kubernetes" {
-  host = "https://${data.terraform_remote_state.k8s.k8s_cluster_endpoint}"
+  host = "https://${data.terraform_remote_state.k8s.outputs.k8s_cluster_endpoint}"
   token = data.digitalocean_kubernetes_cluster.main.kube_config[0].token
   cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
 }
 
 module "deployment" {
   source = "../common/deployment"
-  database_uri = data.terraform_remote_state.pgsql_db.pgsql_db_uri
+  database_uri = data.terraform_remote_state.pgsql_db.outputs.pgsql_db_uri
 }
