e60abdd41962789977148e3857bb54679aed4f2a
Shukant Pal <sukantk2002@outlook.com>
2022-10-23 23:00:02 -0400

    delete stupid vopc

--- common/postgresql/digitalocean/main.tf
+++ common/postgresql/digitalocean/main.tf
@@ -1,17 +1,3 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    endpoint = "https://${var.region}.digitaloceanspaces.com"
-    region = "us-east-1"
-    bucket = var.backend_bucket
-    key = "vpc.terraform.tfstate"
-    access_key = var.spaces_access_id
-    secret_key = var.spaces_secret_key
-    skip_credentials_validation = true
-    skip_region_validation = true
-  }
-}
-
 resource "digitalocean_database_cluster" "main" {
   name = "${var.installation}-db-cluster"
   engine = "pg"
@@ -19,7 +5,6 @@ resource "digitalocean_database_cluster" "main" {
   size = "db-s-1vcpu-1gb"
   region = var.region
   node_count = 1
-  private_network_uuid = data.terraform_remote_state.vpc.outputs.vpc_uuid
 }
 
 resource "digitalocean_database_db" "main" {
--- common/vpc/digitalocean/main.tf
+++
@@ -1,5 +0,0 @@
-resource "digitalocean_vpc" "main" {
-  name   = "${var.installation}-vpc"
-  region = "nyc3"
-  ip_range = "10.10.10.0/24"
-}
