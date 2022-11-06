2e261f5543434de086adea4b0195ee0e696b37a9
Shukant Pal <sukantk2002@outlook.com>
2022-11-06 11:30:25 -0500

    Remove md node pool

--- primitives/cluster/digitalocean/main.tf
+++ primitives/cluster/digitalocean/main.tf
@@ -6,15 +6,7 @@ resource "digitalocean_kubernetes_cluster" "main" {
   node_pool {
     name = "${var.installation}-k8s-node-pool"
     size = var.k8s_node_pool_size
-    node_count = 2
+    min_nodes = 1
+    max_nodes = 10
   }
-}
-
-resource "digitalocean_kubernetes_node_pool" "medium" {
-  cluster_id = digitalocean_kubernetes_cluster.main.id
-  name = "${var.installation}-md"
-  size = "g-2cpu-8gb"
-  min_nodes = 0
-  max_nodes = 9
-  auto_scale = true
 }
