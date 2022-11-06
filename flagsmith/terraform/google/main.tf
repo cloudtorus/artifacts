0db46cfd6d8ea6a636a25bbd6d9f5308380d1504
Shukant Pal <sukantk2002@outlook.com>
2022-11-05 21:14:10 -0400

    host -> endpoint

--- flagsmith/terraform/google/main.tf
+++ flagsmith/terraform/google/main.tf
@@ -23,14 +23,14 @@ data "google_container_cluster" "main" {
 }
 
 provider "kubernetes" {
-  host = "https://${data.terraform_remote_state.cluster.outputs.host}"
+  host = "https://${data.terraform_remote_state.cluster.outputs.endpoint}"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.main.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host = "https://${data.terraform_remote_state.cluster.outputs.host}"
+    host = "https://${data.terraform_remote_state.cluster.outputs.endpoint}"
     token = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.main.master_auth[0].cluster_ca_certificate)
   }
