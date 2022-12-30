77afbad203f2622dfbcf8a9c41b83f9d67266639
Shukant Pal <SukantK2002@outlook.com>
2022-12-29 20:44:46 -0500

    Move Chatwoot to new Helm workflow

--- chatwoot/terraform/helm/outputs.tf
+++ chatwoot/terraform/helm/outputs.tf
@@ -1,3 +1,14 @@
-output "host" {
+variable "recipe" {
+  type = any
+  default = {}
+}
+
+output "ip_address" {
   value = data.kubernetes_service.chatwoot.status.0.load_balancer.0.ingress.0.ip
+}
+
+output "all" {
+  value = {
+    ip_address = data.kubernetes_service.chatwoot.status.0.load_balancer.0.ingress.0.ip
+  }
 }
