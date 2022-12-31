e798c7bd5edbca3a6f2a66a09207336dab0239f4
Shukant Pal <sukantk2002@outlook.com>
2022-12-31 13:41:37 -0500

    Set result to password

--- ghost/terraform/digitalocean/outputs.tf
+++ ghost/terraform/digitalocean/outputs.tf
@@ -2,7 +2,7 @@ output "mysql-innodbcluster" {
   value = {
     credentials = {
       root = {
-        password = random_password.mysql_password
+        password = random_password.mysql_password.result
       }
     }
   }
