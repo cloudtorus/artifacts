d182a08012c719796361d772ca3d07e95cd46e75
Shukant Pal <sukantk2002@outlook.com>
2022-12-30 22:15:35 -0500

    downgrade to k8s 1.24

--- primitives/cluster/terraform/digitalocean/main.tf
+++ primitives/cluster/terraform/digitalocean/main.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "main" {
   name    = "torus-${var.context.id}"
   region  = var.context.region
-  version = "1.25.4-do.0"
+  version = "1.24.8-do.0"
 
   node_pool {
     name       = "torus-${var.context.id}-node-pool"
